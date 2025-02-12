e0eb082146f586aff9ce7b8a9275764f9445b7b2
sebaspv <poncegramming@gmail.com>
2025-02-12 16:11:20 -0600

    get shortest network path

--- mix.exs
+++ mix.exs
@@ -23,7 +23,8 @@ defmodule Lambdanet.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-      {:csv, "~> 3.2"}
+      {:csv, "~> 3.2"},
+      {:libgraph, "0.16.0"}
     ]
   end
 end
--- test/csv_parse_test.exs
+++ test/csv_parse_test.exs
@@ -3,30 +3,34 @@ require File
 require Path
 require CSV
 require Map
+require Graph
 
 defmodule ParseCsv do
     use ExUnit.Case
     doctest Lambdanet
 
     def add_to_adj(start_node, end_node, adj_list) do
-        adj_list[start_node] ++ [end_node]
+        new_adj = adj_list[start_node] ++ [end_node]
+        new_adj
     end
 
-    def populate_graph vertices do
-        for vertex <- vertices do
-            vertex
-        end
+    def parse_csv file_name do
+        file = File.stream!(file_name)
+        file |> CSV.decode(headers: true) |> Enum.to_list
+    end
+
+    def populate_graph parsed_enum do
+        Enum.map(parsed_enum, fn x -> {String.to_atom(elem(x, 1)["Start"]), String.to_atom(elem(x, 1)["End"]), weight: String.to_integer(elem(x, 1)["Weight"])} end)
     end
 
     test "parses csv" do
-        file = File.stream!("test/csv_link_test.csv")
-        final = file |> CSV.decode(headers: true) |> Enum.to_list
-        aux = []
-        for row <- final do
-            edge = {elem(row, 1)["Start"], elem(row, 1)["End"], elem(row, 1)["Weight"], elem(row, 1)["Capacity"]}
-            add_to_adj(elem(edge, 0), elem(edge, 1), aux)
-            IO.inspect edge
-        end
+        final = parse_csv "test/csv_link_test.csv"
         assert Enum.all?(final, fn {key, _value} -> key == :ok end)
+    end
+
+    test "populate graph and check shortest distance" do
+        final = parse_csv("test/csv_link_test.csv") |> populate_graph
+        network = Graph.new |> Graph.add_edges(final)
+        assert Graph.dijkstra(network, :A, :C) == [:A, :B, :C]
     end
 end
