760817660a76ca6a2aa512460532d419a1baef3a
sebaspv <poncegramming@gmail.com>
2025-02-12 02:06:25 -0600

    parse as tuples

--- test/csv_parse_test.exs
+++ test/csv_parse_test.exs
@@ -7,10 +7,26 @@ require Map
 defmodule ParseCsv do
     use ExUnit.Case
     doctest Lambdanet
-    
-    test "parses csv" do 
+
+    def add_to_adj(start_node, end_node, adj_list) do
+        adj_list[start_node] ++ [end_node]
+    end
+
+    def populate_graph vertices do
+        for vertex <- vertices do
+            vertex
+        end
+    end
+
+    test "parses csv" do
         file = File.stream!("test/csv_link_test.csv")
         final = file |> CSV.decode(headers: true) |> Enum.to_list
+        aux = []
+        for row <- final do
+            edge = {elem(row, 1)["Start"], elem(row, 1)["End"], elem(row, 1)["Weight"], elem(row, 1)["Capacity"]}
+            add_to_adj(elem(edge, 0), elem(edge, 1), aux)
+            IO.inspect edge
+        end
         assert Enum.all?(final, fn {key, _value} -> key == :ok end)
     end
 end
